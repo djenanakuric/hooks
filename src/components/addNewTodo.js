4608b17c819695a6ef41e359010669846cdc5ca1
Djenana Kuric <76661983+djenanakuric@users.noreply.github.com>
2022-01-21 15:53:24 +0100

    Add files via upload

---
+++ src/components/addNewTodo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const addNewTodo = () => {
+    return(
+        <form osSubmit ={}>
+            <label htmlFor="todo">To do</label>
+            <input type='text' id='todo' onChange = {} />
+            <input type='submit' />
+        </form>
+    )
+};
+
+export default addNewTodo;
